73b334e86ee82dc7452390d2d57e255363635f9e
janinene <janinem.usana@gmail.com>
2022-11-30 19:17:17 -0500

    Update index.js for new db route and connection

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 const express = require("express")
+const mongoose = require('mongoose')
+const employRoute = require("./route/employee_route")
+const bodyParser = require('body-parser')
 const cors = require('cors')
 require('dotenv/config')        //allow .env to work
-
-
-const {MongoClient} = require('mongodb');
-
-const client = new MongoClient(process.env.DATABASE_URL)
-const database = client.db('comp3123_assignment2')
-const employees = database.collection('employee')
-
-client.connect()
-console.log('connected to mongodb');
 
 const app = express()
 app.use(cors());
 app.use(express.json())
 
-app.get('/',async(req, res) => {
-    const employee_list = await employees.find().toArray()
-    res.json(employee_list)
+
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then( () => {
+        console.log('DB connected');
+    })
+    .catch( (err) => {
+        console.log('err');
+    });
+
+
+app.use("/employee", employRoute)
+
+
+app.route("/")
+.get((req, res) => {
+    res.send("<h1>Assignment 2</h1> <br> <h2>Stu: Janine</h2>")
 })
 
-app.post('/', async(req, res) => {
-    await employees.insertOne({
-        first_name: 'Strawberry',
-        last_name: "Milkshake",
-        email: "sberry@georgebrow.ca"
-    })
-    res.json('Item was added')
-})
 
 
 const SERVER_PORT = process.env.PORT || 8080

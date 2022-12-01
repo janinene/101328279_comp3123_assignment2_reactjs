24364bea1f322ba24fbd2e2afaaf6768c29176a3
janinene <janinem.usana@gmail.com>
2022-11-30 19:51:56 -0500

    Update index, added user route

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express")
 const mongoose = require('mongoose')
+
 const employRoute = require("./route/employee_route")
+const userRoute = require("./route/user_route")
+
 const bodyParser = require('body-parser')
 const cors = require('cors')
 require('dotenv/config')        //allow .env to work
+
 
 const app = express()
 app.use(cors());
@@ -20,6 +24,7 @@ mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTop
 
 
 app.use("/employee", employRoute)
+app.use("/user", userRoute)
 
 
 app.get('/', (req, res) => {

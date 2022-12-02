e60a4d1b358cd33a24c46a4676e4e0ef0eeee21d
janinene <janinem.usana@gmail.com>
2022-12-02 17:35:30 -0500

    Updated employee list key for delete

--- 101328279_comp3123_assignment2_reactjs/src/pages/keys/employee_list_key.js
+++ 101328279_comp3123_assignment2_reactjs/src/pages/keys/employee_list_key.js
@@ -1,7 +1,28 @@
 import React from 'react'
 import {  Link } from "react-router-dom";
+import axios from 'axios';
 
 export default function employee_list_key(props) {
+    
+    // Get User By ID
+    const deleteUserDataByID = (id) => {
+        // let { id } = useParams();
+        axios.delete(`https://comp3123-101328279-assignment2.herokuapp.com/api/employee/${id}`)
+        .then(res =>  { 
+            console.log(res.data)
+            let employList = [props.employees.first_name,props.employees.last_name,props.employees.email]
+            let employeeList = employList.filter(u => {
+                return u.id !== id
+
+            })
+
+            this.setState({employees: employeeList})
+            
+        })
+    }
+
+
+    
   return (   
         <>   
             <tr>
@@ -15,8 +36,9 @@ export default function employee_list_key(props) {
                     <Link to={"/edit/"+props.employees.id}>
                         <button type="submit" className="btn btn-success cancel">Edit</button>                  
                     </Link> 
-                    <Link to={"/list/" + props.employees.id}>
-                        <button type="submit" className="btn btn-danger cancel">Delete</button>             
+                             
+                    <Link to={"/list/"}>                                                                                              {/* to refresh the page v */}
+                        <button type="submit" className="btn btn-danger cancel" onClick={ (e) => {deleteUserDataByID(props.employees.id); window.location.reload(false); }}>Delete</button>        
                     </Link>                     
                     
                 </td>

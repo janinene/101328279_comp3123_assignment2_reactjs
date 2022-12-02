47508fb0914681f3df3d046dc29852418344c657
janinene <janinem.usana@gmail.com>
2022-12-01 19:46:02 -0500

    updated app.js

--- 101328279_comp3123_assignment2_reactjs/src/App.js
+++ 101328279_comp3123_assignment2_reactjs/src/App.js
@@ -6,19 +6,34 @@ import EmployeeList from './pages/EmployeeList';
 import AddEmployee from './pages/AddEmployee';
 import Navigation from './pages/Navigation';
 import Home from './pages/Home';
+import UserLogin from './pages/UserLogin';
+import SignUp from './pages/SignUp';
+
 
 function App() {
   return (
     <>  
+      {/* <UserLogin /> */}
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navigation/>} >
-            <Route index element={<Home/>} />
-            <Route path="list"element={<EmployeeList/>} />
-            <Route path="add" element={<AddEmployee/>} />
-          </Route>
+            <Route path="/" element={<Navigation/>} >
+              <Route index element={<Home/>} />
+              <Route path="list"element={<EmployeeList/>} />
+              <Route path="add" element={<AddEmployee/>} />
+            </Route>
         </Routes>
       </BrowserRouter>
+      {/* <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<UserLogin/>} />
+          <Route path="signup"element={<SignUp/>} />
+            <Route path="/" element={<Navigation/>} >
+              <Route index element={<Home/>} />
+              <Route path="list"element={<EmployeeList/>} />
+              <Route path="add" element={<AddEmployee/>} />
+            </Route>
+        </Routes>
+      </BrowserRouter> */}
     </>
   );
   

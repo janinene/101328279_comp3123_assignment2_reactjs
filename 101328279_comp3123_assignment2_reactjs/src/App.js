6ff990bcdeb8632baf51e1c3cc65fcea7614f673
janinene <janinem.usana@gmail.com>
2022-12-02 23:03:51 -0500

    Updated APP, clean code

--- 101328279_comp3123_assignment2_reactjs/src/App.js
+++ 101328279_comp3123_assignment2_reactjs/src/App.js
@@ -1,7 +1,7 @@
+import { Outlet, Routes, Route } from "react-router-dom";
+import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";      // to add bootstrap
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import EmployeeList from './pages/EmployeePage/EmployeeList';
 import AddEmployee from './pages/EmployeePage/AddEmployee';
 import Navigation from './pages/EmployeePage/Navigation';
@@ -13,34 +13,26 @@ import SignUp from './pages/UserPage/SignUp';
 
 
 function App() {
+
   return (
-    <>  
-      {/* <UserLogin /> */}
-      <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Navigation/>} >
-              <Route index element={<Home/>} />
-              <Route path="list" element={<EmployeeList/>} />
-              <Route path="view/:id" element={<View/>} />
-              <Route path="edit/:id" element={<Edit/>} />
-              <Route path="add" element={<AddEmployee/>} />
-            </Route>
-        </Routes>
-      </BrowserRouter>
-      {/* <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<UserLogin/>} />
-          <Route path="signup"element={<SignUp/>} />
-            <Route path="/" element={<Navigation/>} >
-              <Route index element={<Home/>} />
-              <Route path="list"element={<EmployeeList/>} />
-              <Route path="add" element={<AddEmployee/>} />
-            </Route>
-        </Routes>
-      </BrowserRouter> */}
+    <>
+      <Routes>
+        <Route path="/" index element={<UserLogin />} />
+        <Route path="/login" element={<UserLogin />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={<Navigation />} >
+          <Route path="home" element={<Home />} />
+          <Route path="list" element={<EmployeeList />} />
+          <Route path="view/:id" element={<View />} />
+          <Route path="edit/:id" element={<Edit />} />
+          <Route path="add" element={<AddEmployee />} />
+          <Route path="*" element={<Home />} />
+        </Route>
+      </Routes>
+      <Outlet />
     </>
   );
-  
+
 }
 
 export default App;
